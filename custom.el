cce29fb95be9caa6c260ede8296a4e2ee158e974
mega-haste <www.hichamhot@gmail.com>
2025-02-04 15:31:02 +0100

    First Commit

---
+++ custom.el
@@ -0,0 +1,18 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("88f7ee5594021c60a4a6a1c275614103de8c1435d6d08cc58882f920e0cec65e" "e4a702e262c3e3501dfe25091621fe12cd63c7845221687e36a79e17cf3a67e0" "ffafb0e9f63935183713b204c11d22225008559fa62133a69848835f4f4a758c" "48042425e84cd92184837e01d0b4fe9f912d875c43021c3bcb7eeb51f1be5710" "691d671429fa6c6d73098fc6ff05d4a14a323ea0a18787daeb93fde0e48ab18b" "4594d6b9753691142f02e67b8eb0fda7d12f6cc9f1299a49b819312d6addad1d" default))
+ '(doom-modeline-always-show-macro-register t)
+ '(doom-modeline-height 30)
+ '(elfeed-feeds '("http://www.reddit.com/r/ReverseEngineering/.rss"))
+ '(package-selected-packages '(org-roam-ui rainbow-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(put 'customize-group 'disabled nil)
